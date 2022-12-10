6f9f7c361d85c58a822b83756d53fd123f2f3b3b
AUNG LIN OO <117843549+aunglinoo31@users.noreply.github.com>
2022-12-10 03:09:28 +0300

    Add files via upload

---
+++ C PROGRAMMING HOMEWORK 1.0/C PROGRAMMING HOMEWORK 1.0/main num 13.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number, R;
+    printf("Enter your number=\t");
+    scanf("%d", &number);
+    R = number * 6 ;
+    printf("%d", R);
+    return 0;
+}
+
+/* #include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num= 6, i , R;
+    for (i=1;i<=12;i++)
+    {
+        scanf("%d", &i);
+        R = num * i;
+        printf("%d\n", R);
+    }
+    return 0;
+}
+*/
